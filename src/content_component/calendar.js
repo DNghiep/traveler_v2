c6d84e27fad82cff890f26d81ec02fa110582134
Nguyễn Bá Nam <nguyenbanam98@gmail.com>
2019-04-22 16:37:22 +0700

    Add calendar

--- src/content_component/calendar.js
+++ src/content_component/calendar.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
+function renderDay(day) {
+  const date = day.getDate();
+  const cellStyle = {
+    height: 40,
+    width: 95,
+    position: 'relative',
+    // chỗ này để chính size từng ô
+  };
+  return (
+    <div style={cellStyle}>
+      {date}
+    </div>
+  );
+}
 
 class Calendar extends Component{
-    render(){
-        return(
-                <div>
-                    <h1>Calendar</h1>
-                </div>
-        )
-    }
+  render(){
+    return <DayPicker renderDay={renderDay}/>;
+  }
 }
 
 export default Calendar;
