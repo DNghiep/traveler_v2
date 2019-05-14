1d4b69d7ee24905e46f02c5be378eae73010b291
nquangcuong96 <49380380+nquangcuong96@users.noreply.github.com>
2019-05-14 15:55:01 +0700

    Update Account_manager.js and add Account_manager.css

--- src/Component/Account_manager/Account_manager.js
+++ src/Component/Account_manager/Account_manager.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 import ManagerPage from './pages/ManagerPage';
-import './SignIn_SignUp.css';
+import './Account_manager.css';
 
 class Account_manager extends Component {
   render() {
     return (
       <Router basename="/react-auth-ui/">
-        <div className="App">
-          <div className="manager-body">
-          <p className="title"> account infomation </p>
-          <ManagerPage />
+        <div className="app">
+          <div className="header">  < /div>
+          <div className="body">
+            <div className="manager-body">
+            <p className="title"> account infomation </p>
+            <ManagerPage />
+            </div>
           </div>
+          <div className="footer"> < /div>
         </div>
       </Router>
     );
